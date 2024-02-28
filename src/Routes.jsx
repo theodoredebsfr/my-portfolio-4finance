baa581ff848115466d56707fe50f1a1908164d2b
theodore debs <theodoredebs+fr@gmail.com>
2024-02-28 20:05:28 +0100

    connecting about us page to its route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,6 +6,7 @@ import NotFound from "./pages/notFound/NotFound";
 import { Route, Routes } from "react-router-dom";
 import Counter from "./pages/counter/Counter";
 import RootLayout from "./layout/rootLayout/RootLayout";
+import AboutUs from "./pages/about-us/AboutUs";
 
 const MainRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const MainRoutes = () => {
         <Route path="" element={<Home />} />
         <Route path="blogs" element={<Blogs />} />
         <Route path="contact-us" element={<Contact />} />
+        <Route path="about-us" element={<AboutUs />} />
         <Route path="counter" element={<Counter />} />
         <Route path="*" element={<NotFound />} />
       </Route>
--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -11,6 +11,9 @@ const Header = () => {
             <Link to="">Home</Link>
           </li>
           <li>
+            <Link to="/about-us">About US</Link>
+          </li>
+          <li>
             <Link to="/blogs">Blogs</Link>
           </li>
           <li>
